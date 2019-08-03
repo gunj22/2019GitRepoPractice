4f81045e81f8b7dbd4a6921919d34fdc703a832e
gunj22 <sam_bhasin4@yahoo.com>
2019-08-03 21:21:22 +0530

    2019GitCommandPractice

---
+++ src/main/java/GitPractice2019/GitPractice2019/App.java
@@ -0,0 +1,13 @@
+package GitPractice2019.GitPractice2019;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
